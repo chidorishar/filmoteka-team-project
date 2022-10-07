a580544a9dacd5cdc9bf8bd72e52e7fcffa22f8e
korzhenkovitalii <korzhenko.vitalii@gmail.com>
2022-10-08 00:36:39 +0300

    Update library.js

--- src/library.js
+++ src/library.js
@@ -1,1 +1,21 @@
 // import './js/***.js';
+class LocalStorageUtil {
+  constructor({ name, date, poster, genre }) {
+    this.keyName = 'films';
+  }
+
+    getFilms() {
+        const filmsLocalStorage = localStorage.getItem(this.keyName);
+        if (filmsLocalStorage !== null) {
+            return JSON.parse(filmsLocalStorage);
+        }
+        return []
+  }
+
+  getFilms() {}
+}
+
+const localStorageUtil = new LocalStorageUtil();
+
+const a = localStorageUtil.getFilms();
+console.log(a)
