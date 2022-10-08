410f2661dbae09d38cd2d29c1cafbf3def5590d0
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-10-08 18:39:55 +0300

    Added rendering of top movies from tmdb backend

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -1,4 +1,50 @@
 import { GalleryAPI } from './galleryAPI';
+import { TMDBAPI } from './theMovieAPI';
+
+const GENRES_DATA_LS_KEY = 'genres-data';
+
+let moviesData = null;
+let tmdbAPI = null;
 
 // MAIN
-(() => {})();
+(async () => {
+  try {
+    tmdbAPI = new TMDBAPI();
+    const pathToPosterImg = (await tmdbAPI.getConfiguration()).images.base_url;
+    const genresDataFromLS = readFromLocalStorage(GENRES_DATA_LS_KEY);
+    moviesData = (await tmdbAPI.getTopMovies()).results; //(await tmdbAPI.getMoviesByName('test')).results;
+    //get array of IDs and genres
+    let genresAndIDs = null;
+    if (!genresDataFromLS) {
+      const genresDataFromBackend = (await tmdbAPI.getGenresData()).genres;
+
+      genresAndIDs = genresDataFromBackend;
+      localStorage.setItem(
+        GENRES_DATA_LS_KEY,
+        JSON.stringify(genresDataFromBackend)
+      );
+    } else {
+      genresAndIDs = genresDataFromLS;
+    }
+    const galleryAPI = new GalleryAPI(
+      '#movies-wrapper',
+      pathToPosterImg,
+      genresAndIDs
+    );
+
+    //render images
+    galleryAPI.renderMoviesCards(moviesData);
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
+
+function readFromLocalStorage(key) {
+  try {
+    const item = localStorage.getItem(key);
+
+    return JSON.parse(item);
+  } catch (error) {
+    return null;
+  }
+}
