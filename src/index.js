7d31368f7993759b8b0e73f96a5099c3ef0e8898
Надія Ткач <nadiya77723@gmail.com>
2022-10-08 11:21:19 +0300

    додає кольори в палітру,приховує спінер

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // import './js/***.js';
 
-let maskEl = document.querySelector('.mask');
+// let maskEl = document.querySelector('.mask');
 
-window.addEventListener('load', () => {
- maskEl.classList.add('hide');
- setTimeout(() => {
-    maskEl.remove();
- }, 600);
-});
+// window.addEventListener('load', () => {
+//   maskEl.classList.add('hide');
+//   setTimeout(() => {
+//     maskEl.remove();
+//   }, 600);
+// });
