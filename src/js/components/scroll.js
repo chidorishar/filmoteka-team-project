17bf37f98d10beac1b458fc539ddf7872413b548
Vlad <40580001+Javist01@users.noreply.github.com>
2022-10-11 19:11:27 +0300

    пофиксил

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', onScrollClick);
 
 function onScrollClick(e) {
   e.preventDefault();
-  if (window.scrollY > 320) {
+  if (window.scrollY < 320) {
     scrollBtn.classList.remove('scroll--isShown');
   } else {
     scrollBtn.classList.add('scroll--isShown');
