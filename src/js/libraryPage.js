a6a689b0a38e803494938e06729119ab7ac11b36
MiroProger <sirmiroslavsuprun@gmail.com>
2022-10-12 20:44:04 +0300

    Deleted unnecessary code from the branch

--- src/js/libraryPage.js
+++ src/js/libraryPage.js
@@ -1,8 +1,8 @@
 import './components/teamModalWindow.js';
 import { GalleryAPI } from './components/GalleryAPI';
+import { TMDBAPI } from './libs/TMDBAPI';
 import { LDStorageAPI } from './utils/LibraryDataStorageAPI';
-import { BackendConfigStorage } from './libs/BackendConfigStorage.js';
-import { MovieModalHandler } from './components/MovieModalHandler';
+import { readFromLocalStorage } from './utils/WebStorageMethods';
 import {
   renderPagination,
   paginationNextBtn,
@@ -11,87 +11,39 @@ import {
   pagination,
   onWindowResize,
 } from './components/pagination';
+
+const GENRES_DATA_LS_KEY = 'genres-data';
+const MOVIE_INFO = {
+  WATCHED: 'watched',
+  QUEUED: 'queued',
+};
+
 let galleryAPI = null;
-// const libraryWatchedBtn = document.getElementById('library-watched');
-// const libraryQueueBtn = document.getElementById('library-queue');
 
 // MAIN
 (async () => {
   try {
-    //init
+    // All Storage API methods
     LDStorageAPI.init();
-    await BackendConfigStorage.init();
-    galleryAPI = new GalleryAPI('#movies-wrapper');
-    const mmh = new MovieModalHandler(
-      '#watched-btn',
-      '#queue-btn',
-      '#movies-modal-window',
-      '.modal-close',
-      '#movie-modal-buttons-wrapper',
-      galleryAPI
-    );
+    LDStorageAPI.setActiveStorage(MOVIE_INFO.WATCHED);
+    LDStorageAPI.getTotalPages();
+    LDStorageAPI.getMoviesByPage(1);
+    console.log(1);
 
-    pagination.totalPages = LDStorageAPI.getTotalPages();
+    const tmdbAPI = new TMDBAPI();
+    const pathToPosterImg = (await tmdbAPI.getConfiguration()).images
+      .secure_base_url;
+    const genresAndIDs = readFromLocalStorage(GENRES_DATA_LS_KEY);
+
+    galleryAPI = new GalleryAPI(
+      '#movies-wrapper',
+      pathToPosterImg,
+      genresAndIDs
+    );
 
     //render movies
-    galleryAPI.renderMoviesCards(LDStorageAPI.getMoviesByPage(1));
-    renderPagination();
-
-    // Event listeners
-    paginationNextBtn.addEventListener('click', onPaginationBtnChangeClick);
-    paginationPreviousBtn.addEventListener('click', onPaginationBtnChangeClick);
-    paginationPagesList.addEventListener(
-      'click',
-      onPaginationListBtnNumberClick
-    );
-    window.addEventListener('resize', onWindowResize);
-    // libraryWatchedBtn.addEventListener('click', onLibraryMainBtnClick);
-    // libraryQueueBtn.addEventListener('click', onLibraryMainBtnClick);
+    // galleryAPI.renderMoviesCards(moviesData);
   } catch (error) {
     console.log(error);
   }
 })();
-
-function onPaginationBtnChangeClick(e) {
-  if (e.currentTarget.id === 'pagination-button-next') {
-    pagination.currentPageIncreaseByOne();
-  } else {
-    pagination.currentPageReduceByOne();
-  }
-
-  renderMovieGalleryByPagePagination();
-
-  renderPagination();
-}
-
-function onPaginationListBtnNumberClick(e) {
-  if (e.target.nodeName !== 'BUTTON') return;
-  if (parseInt(e.target.textContent) === pagination.currentPage) return;
-
-  pagination.updateCurrentPage(parseInt(e.target.textContent));
-
-  renderMovieGalleryByPagePagination();
-
-  renderPagination();
-}
-
-function renderMovieGalleryByPagePagination() {
-  // In case of removing movies from our Local Storage
-  pagination.totalPages = LDStorageAPI.getTotalPages();
-
-  galleryAPI.renderMoviesCards(
-    LDStorageAPI.getMoviesByPage(pagination.currentPage)
-  );
-}
-
-// function onLibraryMainBtnClick(e) {
-//   if (e.target.id === 'library-watched') {
-//     LDStorageAPI.setActiveStorage = LDStorageAPI.MOVIE_INFO.WATCHED;
-//   } else {
-//     LDStorageAPI.setActiveStorage = LDStorageAPI.MOVIE_INFO.QUEUED;
-//   }
-//   pagination.currentPage = 1;
-
-//   renderMovieGalleryByPagePagination();
-//   renderPagination();
-// }
