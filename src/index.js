79bb6f262261fdf76edbe3be31d7ca4d26cf051b
Ilya Bondarchuk <cod3provider@gmail.com>
2022-10-13 19:35:26 +0300

    fix some bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import './js/***.js';
 
-import './js/sign-in';
+import './js/components/sign-in';
 import './js/mainPage.js';
 import './js/utils/LibraryDataStorageAPI.js';
 import './js/components/teamModalWindow.js';
