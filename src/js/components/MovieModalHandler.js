318cde82c55d8f0462987ad3cffe4c12b50ebcc1
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-10-15 12:22:42 +0300

    add poster-placeholder to modal window

--- src/js/components/MovieModalHandler.js
+++ src/js/components/MovieModalHandler.js
@@ -132,6 +132,27 @@ export class MovieModalHandler {
     refs.movieGenre.textContent = `${
       movieGenresString ? movieGenresString : 'N/D'
     }`;
+
+    function addPosterPlaceholder() {
+      const modalPosterPlaceholder =
+        document.getElementById('modal-placeholder');
+      const textInPlaceholder = document.querySelector(
+        '.modal-poster__placeholder--title'
+      );
+      const hiddenPoster = document.querySelector('.poster');
+
+      if (!pathToPoster) {
+        modalPosterPlaceholder.classList.remove('is-hidden');
+        textInPlaceholder.textContent = `${movieTitle ?? movieName}`;
+        hiddenPoster.classList.add('is-hidden');
+      } else {
+        modalPosterPlaceholder.classList.add('is-hidden');
+        hiddenPoster.classList.remove('is-hidden');
+      }
+    }
+
+    addPosterPlaceholder();
+
     refs.movieAbout.textContent = `${overview ? overview : 'Sorry, no data'}`;
     //update text in library-related buttons according to movie's persistance in a library
     this.#updateControlButtons(this.#movieLibData);
