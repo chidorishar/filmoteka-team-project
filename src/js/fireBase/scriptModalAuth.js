9643ffccd14f030f6f0cb543a8e9303182d6a423
Ilya Bondarchuk <cod3provider@gmail.com>
2022-10-16 10:42:12 +0300

    small fixes and clean trash

--- src/js/fireBase/scriptModalAuth.js
+++ src/js/fireBase/scriptModalAuth.js
@@ -1,18 +1,3 @@
-// const signInBtn = document.querySelector('.signin-btn');
-// const signUpBtn = document.querySelector('.signup-btn');
-// const formBox = document.querySelector('.form-box');
-// const overlayModal = document.querySelector('.overlay-modal');
-//
-// signUpBtn.addEventListener('click', function () {
-//   formBox.classList.add('active');
-//   overlayModal.classList.add('active');
-// });
-//
-// signInBtn.addEventListener('click', function () {
-//   formBox.classList.remove('active');
-//    overlayModal.classList.remove('active');
-// });
-
 const refs = {
   signInBtn: document.querySelector('.signin-btn'),
   signUpBtn: document.querySelector('.signup-btn'),
